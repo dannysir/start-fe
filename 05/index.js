a7243188afeafa605004cdec76c3026a86d4fbf5
Seo San <dannysir@naver.com>
2023-04-05 18:24:20 +0900

    checkerBoard&img

---
+++ 05/index.js
@@ -0,0 +1,28 @@
+// const wrap = document.getElementById("wrap");
+// const item = document.querySelector(".item");
+// console.log(wrap);
+// console.log(item);
+
+// const element = document.querySelectorAll("div");
+// console.log(element);
+
+// const iteminDIV = document.querySelector("#wrap .item");
+
+// const div = document.createElement("div");
+// div.style.border = "1px solid red";
+// div.innerHTML = "hello!!";
+
+// //document.body.appendChild(div);
+// //전부 다 바뀜
+// document.body.innerHTML = "1111";
+
+// const span = document.createElement("span");
+// const textNode = document.createTextNode("hello!");
+// span.appendChild(textNode);
+// document.querySelector("#debug").appendChild(span);
+
+// 실무에서 dom 객체는 이름 앞에 $표시를 해준다.!!!!!!!!!!
+
+// window.addEventListener("click", () => {
+//   console.log("window load event");
+// });
