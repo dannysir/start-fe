e3044e6f0cd77e60e243c4e71fc21f939b147394
Seo San <dannysir@naver.com>
2023-04-19 23:54:14 +0900

    login

--- 07/login.js
+++ 07/login.js
@@ -1,23 +1,27 @@
-//const $id = document.querySelector("[name = id]");
-const $id = document.getElementsByName("id");
+const $id = document.querySelector("[name = id]");
 const $pw = document.querySelector("[name = pw]");
 const $btn = document.querySelector("button");
 const $form = document.querySelector("form");
+const $result = document.querySelector("#result");
+// 입력한 id,pw가 맞을경우엔 success , 아니면 error출력
+const realId = "hello";
+const realPw = "world";
 
 const loginFun = (event) => {
   event.preventDefault();
   const id = $id.value;
   const pw = $pw.value;
 
-  saveName(id);
+  saveName(id, pw);
 };
 
-const saveName = (name) => {
-  console.log(name);
+const saveName = (name, password) => {
+  console.log(name, password);
+  if (name == realId && password == realPw) {
+    $result.innerHTML = "<div class = success>success</div>";
+  } else {
+    $result.innerHTML = "<div class = error>error</div>";
+  }
 };
 
-$form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log($form);
-  console.log($pw.value);
-});
+$form.addEventListener("submit", loginFun);
