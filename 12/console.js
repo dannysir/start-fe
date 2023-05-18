35de756c1cfa475f4c423305cb8f8e0754e38a5b
Seo San <dannysir@naver.com>
2023-05-18 16:54:18 +0900

    no message

---
+++ 12/console.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+function san(msg) {
+  console.log(msg);
+}
+export { san };
+//module.exports = console.log;
---
+++ 12/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable spaced-comment */
+//import { san } from "./console";
+
+const prettyBytes = require("pretty-bytes");
+
+console.log(prettyBytes(100));
+console.log(prettyBytes(1337));
